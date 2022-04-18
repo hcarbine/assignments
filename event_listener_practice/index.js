1c078614391cb8a542d127eefae8ff60fed05a71
Holly Carbine <hcarbine@gmail.com>
2022-04-17 22:37:55 -0600

    add all box element functions

--- event_listener_practice/index.js
+++ event_listener_practice/index.js
@@ -5,3 +5,19 @@ document.getElementById("box").addEventListener("mouseover", function() {
 document.getElementById("box").addEventListener("mousedown", function() {
     document.getElementById("box").style.backgroundColor = "red";
 });
+
+document.getElementById("box").addEventListener("mouseup", function() {
+    document.getElementById("box").style.backgroundColor = "yellow";
+});
+
+document.getElementById("box").addEventListener("dblclick", function() {
+    document.getElementById("box").style.backgroundColor = "green";
+});
+
+document.getElementById("box").addEventListener("wheel", function() {
+    document.getElementById("box").style.backgroundColor = "orange";
+});
+
+document.body.addEventListener("wheel", function() {
+    document.getElementById("box").style.backgroundColor = "orange";
+});
