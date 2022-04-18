3b8881b41c155ffe4ae79593c970c905e82b58f5
Holly Carbine <hcarbine@gmail.com>
2022-04-17 21:41:23 -0600

    add mouseover and mousedown

---
+++ event_listener_practice/index.js
@@ -0,0 +1,7 @@
+document.getElementById("box").addEventListener("mouseover", function() {
+    document.getElementById("box").style.backgroundColor = "blue";
+});
+    
+document.getElementById("box").addEventListener("mousedown", function() {
+    document.getElementById("box").style.backgroundColor = "red";
+});
