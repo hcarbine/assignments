2d5ee0d31256e67d6b78b2528b999da5ba3c60cd
Holly Carbine <hcarbine@gmail.com>
2022-04-18 21:32:19 -0600

    created header

---
+++ dom-manipulation-olympics/index.js
@@ -0,0 +1,7 @@
+const header = document.getElementById("header");
+header.innerHTML = "<h1>JavaScript Made This!!</h1><h2><span style='color:peachpuff'>Holly</span> wrote this JavaScript</h2>"
+
+header.style.display = "flex"
+header.style.alignItems = "center"
+header.style.flexDirection = "column"
+
