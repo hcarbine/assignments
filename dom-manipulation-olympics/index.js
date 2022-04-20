7343ea0849a6675b8e4632d6babf446728f3b647
Holly Carbine <hcarbine@gmail.com>
2022-04-20 10:03:14 -0600

    made the clear button function

--- dom-manipulation-olympics/index.js
+++ dom-manipulation-olympics/index.js
@@ -1,7 +1,27 @@
 const header = document.getElementById("header");
-header.innerHTML = "<h1>JavaScript Made This!!</h1><h2><span style='color:peachpuff'>Holly</span> wrote this JavaScript</h2>"
+    header.innerHTML = "<h1>JavaScript Made This!!</h1><h2><span style='color:peachpuff'>Holly</span> wrote this JavaScript</h2>"
 
-header.style.display = "flex"
-header.style.alignItems = "center"
-header.style.flexDirection = "column"
+    header.style.display = "flex"
+    header.style.alignItems = "center"
+    header.style.flexDirection = "column"
+
+const left = document.getElementsByClassName("message left");
+    left[0].textContent = "Hey! What are you doing this weekend?"
+    left[1].textContent = "I need someone to walk my fish!"
+
+const right = document.getElementsByClassName("message right");
+    right[0].textContent = "I'm going to the circus, why?"
+    right[1].textContent = "I hope your fish drowns!"
+
+var clear = document.getElementById("clear-button")
+
+function clearMessages(){
+    left[0].textContent = ""
+    left[1].textContent = ""
+    right[0].textContent = ""
+    right [1].textContent = ""
+    
+}
+clear.addEventListener("click",clearMessages)
+
 
