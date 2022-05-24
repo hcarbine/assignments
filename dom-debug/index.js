7a5b847fdea1807972e9b268f5c30bd733171cba
Holly Carbine <hcarbine@gmail.com>
2022-05-23 23:15:41 -0600

    final commit

--- dom-debug/index.js
+++ dom-debug/index.js
@@ -1,20 +1,23 @@
 const colors = ["red", "blue", "green"]
 
-document.getElementById("add").addEventListener("onclick", function(e){
+
+document.getElementById("add").addEventListener("click", function(e){
+    var input = document.getElementById("input")
     const subItem = createSubItem(e)
     document.getElementById("list").appendChild(subItem)
+    input.value = '';
 })
 
 function createDropDown(){
-    const dropDown = document.createElement("select")
-    for (let i = 0; i < colors; i++){
-        const option = createElement("option") 
+    const dropDown = document.createElement('select')
+    for (let i = 0; i < colors.length; i++){ 
+        const option = document.createElement('option')
         option.innerHTML = colors[i]
         option.value = colors[i]
-        dropDown.append(option)
+        dropDown.appendChild(option)
     }
-    dropDown.addEventListener("onchange", function(e){
-        e.target.parent.backgroundColor = e.target.value
+    dropDown.addEventListener("change", function(e){
+        e.target.parentElement.style.backgroundColor = e.target.value
     })
     return dropDown
 }
@@ -22,7 +25,7 @@ function createDropDown(){
 function createSubItem(e){
     const subItem = document.createElement("div")
     var subItemValue = document.getElementById("input")
-    subItem.textContent = subItemValue
+    subItem.textContent = subItemValue.value
     const dropDown = createDropDown()
     subItem.appendChild(dropDown)
     subItem.setAttribute("class", "subItem")
