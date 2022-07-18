42f4715d5a111fa7036ad23afc504959ea79f59a
Holly Carbine <hcarbine@gmail.com>
2022-07-18 12:22:21 -0600

    Final Commit

---
+++ loops_and_arrays_practice/main.js
@@ -0,0 +1,39 @@
+/*var officeItems = ["stapler", "monitor", "computer", "desk", "lamp", "computer", "lamp", "stapler", "computer", "computer"]
+var count = 0
+for(var i =0; i < officeItems.length; i++){
+    console.log(officeItems[i])
+    if(officeItems[i]=="computer"){
+        count++
+    }
+}
+console.log("There are "+count+" computers.")*/
+
+var peopleWhoWantToSeeMadMaxFuryRoad = [
+    {
+      name: "Mike",
+      age: 12,
+      gender: "male"
+    },{
+      name: "Madeline",
+      age: 80,
+      gender: "female"
+    },{
+      name: "Cheryl",
+      age: 22,
+      gender: "female"
+    },{
+      name: "Sam",
+      age: 30,
+      gender: "male"
+    },{
+      name: "Suzy",
+      age: 4,
+      gender: "female"
+    }
+  ]
+  for(var i = 0; i< peopleWhoWantToSeeMadMaxFuryRoad[i].length; i++) {
+    if(peopleWhoWantToSeeMadMaxFuryRoad.age <=18){
+    console.log(peopleWhoWantToSeeMadMaxFuryRoad[i])
+  }
+}
+  
